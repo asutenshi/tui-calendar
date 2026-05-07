b115f14e643e624437a9b38477c6916a234436f0
Dest1n640 <realmadridfunca@gmail.com>
2026-05-07 14:16:21 +0800

    feat(core): add create_note to the calednar

--- src/tui_calendar/main.py
+++ src/tui_calendar/main.py
@@ -1,6 +1,10 @@
+from datetime import date
+from pathlib import Path
+
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Static, ContentSwitcher
 from tui_calendar.ui.components.month_grid import MonthGrid
+from tui_calendar.core.indexer import NotesIndexer
 
 
 class WeekView(Static):
@@ -37,6 +41,7 @@ class TuiCalApp(App):
         ("m", "switch_view('month')", "Month"),
         ("w", "switch_view('week')", "Week"),
         ("d", "switch_view('day')", "Day"),
+        ("c, n", "create_new", "Create_new")
     ]
 
     def compose(self) -> ComposeResult:
@@ -52,6 +57,18 @@ class TuiCalApp(App):
     def action_switch_view(self, view_id: str) -> None:
         """Переключает текущий вид в ContentSwitcher."""
         self.query_one("#view-switcher", ContentSwitcher).current = view_id
+
+    def on_mount(self) -> None:
+        current_file_path = Path(__file__).resolve()
+        project_root = current_file_path.parent
+        notes_path = project_root.parent.parent / "examples" / "created_notes"
+        self.indexer = NotesIndexer(notes_path)
+        self.selected_date = date.today()
+
+    def action_create_new(self) -> None:
+        """Логика нажатия 'c' или 'n'"""
+        new_file = self.indexer.create_note(self.selected_date, title="Untitled")
+        self.notify(f"Created: {new_file.name}", title="Success", severity="information")
 
 
 def run():
