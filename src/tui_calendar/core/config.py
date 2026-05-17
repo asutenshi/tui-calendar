670a90d6ae4852e5b51de26002d9b315f021b891
Dest1n640 <realmadridfunca@gmail.com>
2026-05-17 22:51:24 +0800

    feat(core): add description for ConfigManager

--- src/tui_calendar/core/config.py
+++ src/tui_calendar/core/config.py
@@ -2,11 +2,22 @@ import yaml
 from pathlib import Path
 
 class ConfigManager: 
+    """Менеджер конфигурации приложения"""
     def __init__(self, folder_path: Path):
+        """
+        Инициализирует менеджер и проверяет наличие файла конфигурации.
+
+        Args:
+            folder_path (Path): Путь к директории, где должен храниться конфиг.
+        """
         self.config_path = folder_path / "config.yaml"
         self._ensure_config_exists()
 
     def _ensure_config_exists(self):
+        """
+        Проверяет, существует ли конфигурационный файл. 
+        Если файла нет, генерирует его с настройками по умолчанию.
+        """
         if self.config_path.exists():
             return False
 
@@ -22,7 +33,13 @@ class ConfigManager:
         with open(self.config_path, 'w', encoding="utf-8") as file:
           yaml.dump(default_data, file, default_flow_style=False)
       
-    def get_tag_color(self):
+    def get_tag_color(self) -> dict:
+        """
+        Считывает конфигурационный файл и возвращает словарь с цветами тегов.
+        Returns:
+            dict: Словарь вида {'tag_name': 'hex_color'}. 
+            Если настройки повреждены или отсутствуют, возвращает пустой словарь {}.
+        """
         with open(self.config_path, 'r', encoding="utf-8") as file:
           config = yaml.safe_load(file)
         if not config:
